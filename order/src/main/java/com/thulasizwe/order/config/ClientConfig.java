0614c9b2cc98221d0da7974460e7b2f887548a5c
Thulasizwe Sabela <thulasizwe.sabela@dariel.co.za>
2025-11-19 14:45:52 +0200

    Decouple adapter from factory

--- order/src/main/java/com/thulasizwe/order/config/ClientConfig.java
+++ order/src/main/java/com/thulasizwe/order/config/ClientConfig.java
@@ -1,7 +1,6 @@
 package com.thulasizwe.order.config;
 
 import com.thulasizwe.order.service.WalletClient;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -11,13 +10,15 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class ClientConfig {
     @Bean
-    WalletClient walletClient(){
+    public WalletClient walletClient(){
         RestClient walletRestClient = RestClient.builder()
                 .baseUrl("http://localhost:8081/v1/api/wallets")
                 .build();
 
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(walletRestClient);
+
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                .builderFor(RestClientAdapter.create(walletRestClient))
+                .builderFor(restClientAdapter)
                 .build();
 
         return factory.createClient(WalletClient.class);
